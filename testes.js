f85a35177823cf11c8dcb208143a2facd690c6d6
azazdeaz <azazdeaz@gmail.com>
2014-07-29 13:42:05 +0200

    work on draft

--- testes.js
+++ testes.js
@@ -20,25 +20,76 @@ module.exports = am;
 
 var am = require('animachine');
 
-function TimelineRow() {
+function CssTimelineRow(src) {
 
+    this._selectors = [];
     this._keyframes = [];
 
     this.domElement = new window.AmguiTimelineRow({color: '#7ED40F'});
 
-    this.domElement.on('selectkeyframe', function (id) {
-
-        var kf = this._keyframes.findOne({id: id});
-    });
+    this._handlerChange = this._handlerChange.bind(this);
 }
 
-TimelineRow.name = 'css';
+CssTimelineRow.name = 'css3';
+
+var p = CssTimelineRow.prototype;
+
+p.select = function () {
+
+    am.handler.reset({type: 'css3'});
+    am.on('change', this._handlerChange);
+}
+
+p.deselect = function () {
+    
+    am.off('change', this._handlerChange);
+}
+
+p._handlerChange = function(prop, value, correct) {
+
+    var time = am.timeline.currTime,
+        keyframe = this._keyframes.findOne({time: time}) || this.createKeyframe(time);
+
+    keyframe.css[prop] = value;
+}
+
+p.addKeyframe = function (src) {
+
+    var keyframe = _.extend({}, src, {
+        time: 0,
+        domElement: document.createElement('am-keyframe'),
+        css: {},
+        attr: {}
+    });
+
+    return keyframe;
+};
+
+p.generateSrc = function () {
+
+    var src = {
+        selectors: _.clone(this._selectors),
+        keyframes: []
+    };
+
+    this._keyframes.forEach(function (keyframe) {
+
+        src.keyframes.push({
+            css: _.clone(keyframes.css),
+            attr: _.clone(keyframes.attr),
+        });
+    })
+}
+
+p.generateAst = function (src) {
+    
+}
 
 var ce = {
 
     init: function () {
 
-        am.timeline.registerRow(TimelineRow);
+        am.timeline.registerRow(CssTimelineRow);
     }
 };
 

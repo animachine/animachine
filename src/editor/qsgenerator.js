baef66af0e8283bd98e002c3b8e7e84630780802
azazdeaz <azazdeaz@gmail.com>
2014-08-13 18:27:14 +0200

    wip

--- src/editor/qsgenerator.js
+++ src/editor/qsgenerator.js
@@ -1,27 +1,121 @@
-function possibleIds(de) {
+function generate(de, root) {
 
-    return de.id ? de.id : [];
-}
+    var deCurr = de,
+        rootCurr = root, 
+        qsCurr, qsParent = '';
 
-function possibleClasses(de) {
+    root = root || document;
 
-    var classes = Array.prototype.slice.call(de.className, 0)
-        .map(function (className) {return '.' + className});
+    while (true) {
 
-    return step(classes, 2);
+        do {
+            qsCurr = gen(deCurr, rootCurr)
+        }
+        while(!qsCurr && 
+            deCurr.parentNode !== rootCurr &&
+            (deCurr = deCurr.parentNode));
 
-    function step(list, back) {
+        if (!qsCurr) {
 
-        var combined = combine(classes, list);
-        return list.concat(back === 0 ? combined : step(combined, --back));
+            if (deCurr.parentNode === rootCurr) {
+
+                qsCurr = '> ' + deCurr.tagName + ':nht-child(' +
+                    Array.prototype.indexOf.call(rootCurr.childNodes, qsCurr) + ')';
+            }
+            else {
+                return; //can't find unique query selector
+            }
+        }
+
+        qsParent += (qsParent ? ' ' : '') + qsCurr;
+
+        if (deCurr === de) {
+
+            return qsParent;
+        }
+        else {
+            qsCurr = undefined;
+            rootCurr = deCurr;
+            deCurr = de;
+        }
     }
 }
 
-function possibleClass(de) {
+function gen(de, root, qsParent) {
 
-    return Array.from(de.className);
+
+    var singles, selectors, matches = [];
+
+    singles = selectors = [de.tagName].concat(
+        possibleIds(de),
+        possibleClasses(de, i),
+        possibleAttributes(de, i)
+    );
+
+    for (var i = 0; i < 5; ++i) {
+
+        selectors.forEach(function (selector) {
+
+            if (root.querySelectorAll(qsParent + ' ' + selector).length === 1) {
+                matches.push(selector);
+            };
+        });
+
+        if (matches.length) {
+
+            break;
+        }
+        else {
+            selectors = combine(selectors, singles);
+        }
+    }
+
+    if (matches.length) {
+
+        return qsParent + ' ' + matches[0];
+    }
+    else if (de.parentNode) {
+
+        qsNewParent = generate(de.parentNode, root, qsParent);
+
+        if (qsNewParent) {
+
+            return generate(de.parent, root, qsParent + ' ' + qsNewParent);
+        }
+    }
 }
 
+function possibleIds(de) {
+
+    return de.id ? [de.id] : [];
+}
+
+function possibleClasses(de, max) {
+
+    return Array.prototype.slice.call(de.classList, 0)
+        .map(function (className) {
+            return '.' + className
+        });
+}
+
+function possibleAttributes(de) {
+
+    return Array.prototype.slice.call(de.attributes, 0)
+        .map(function (attr) {
+            return '[' + attr.name + (attr.value ? '=' + attr.value : '') + ']';
+        });
+}
+
+function variate(_list, length) {
+
+    return step(_list, 2);
+
+    function step(list, back) {
+
+        var combined = combine(attributes, list);
+        return list.concat(back === 0 ? combined : step(combined, --back));
+    }
+}
 
 function combine(sourceA, sourceB) {
 
@@ -29,7 +123,9 @@ function combine(sourceA, sourceB) {
 
     sourceA.forEach(function (a) {
         sourceB.forEach(function (b) {
-            combined.push(a + b);
+            if (a.indexOf(b) === -1 && b.indexOf(a) === -1) {
+                combined.push(a + b);
+            }
         });
     });
 

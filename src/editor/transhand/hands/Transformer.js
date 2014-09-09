0cb5db23a56fb7f3a2453324bda7a56d1192a990
Polgar <azazdeaz@gmail.com>
2014-09-09 07:30:59 +0200

    fix origo moving

--- src/editor/transhand/hands/Transformer.js
+++ src/editor/transhand/hands/Transformer.js
@@ -272,12 +272,12 @@ p._onDrag = function (e) {
             my = pMouse.y - md.pOrigin.y,
             dist = Math.sqrt(mx*mx + my*my),
             r = Math.atan2(my, mx) - params.rz,
-            x = Math.cos(r) * dist * params.sx,
-            y = Math.sin(r) * dist * params.sy;
-        change.ox = params.ox = md.params.ox + (x / params.sx / base.w);
-        change.oy = params.oy = md.params.oy + (y / params.sy / base.h);
-        // change.tx = params.tx = md.params.tx - (x / (params.sx));
-        // change.ty = params.ty = md.params.ty - (y / (params.sy));
+            x = (Math.cos(r) * dist) / params.sx,
+            y = (Math.sin(r) * dist) / params.sy;
+        change.ox = params.ox = md.params.ox + (x / base.w);
+        change.oy = params.oy = md.params.oy + (y / base.h);
+        change.tx = params.tx = md.params.tx + (mx - x);
+        change.ty = params.ty = md.params.ty + (my - y);
 
     }
 };

25fbbd5f2697eb2ad9578901b494ef080645a013
azazdeaz <azazdeaz@gmail.com>
2014-10-22 18:40:51 +0200

    use canvas for making cursors from font

--- src/editor/amgui/amgui.cursor.js
+++ src/editor/amgui/amgui.cursor.js
@@ -17,21 +17,52 @@ function createCursorFromText(opt) {
     opt = opt || {};
     opt.width = opt.width || 24;
     opt.height = opt.height || 24;
-    opt.cx = opt.cx || opt.width / 2;
-    opt.cy = opt.cy || opt.height / 2;
-    opt.tx = opt.tx || 0;
-    opt.ty = opt.ty || opt.height / 2;
+    opt.hotspotX = opt.hotspotX || opt.width / 2;
+    opt.hotspotY = opt.hotspotY || opt.height / 2;
+    opt.textX = opt.textX || 0;
+    opt.textY = opt.textY || 0;
     opt.fontFamily = opt.fontFamily || 'amgui';
     opt.fontSize = opt.fontSize || opt.height / 2 + 'px';
     opt.color = opt.color || '#000';
-    opt.r = opt.r || 0;
+    opt.rotate = opt.rotate || 0;
+    opt.rotateOriginX = opt.rotateOriginX || 0;
+    opt.rotateOriginY = opt.rotateOriginY || 0;
     opt.text = opt.text || 'A';
-
-    var svg = 'url(\'data:image/svg+xml;utf8,'
-        + '<svg xmlns="http://www.w3.org/2000/svg" width="'+opt.width+'" height="'+opt.height+'">'
-        + '<text x="'+opt.ty+'" y="'+opt.ty+'" transform="rotate('+opt.r+', '+opt.cx+', '+opt.cy+')" '
-        + 'fill="'+opt.color+'" font-size="'+opt.fontSize+'" font-family="'+opt.fontFamily+'">'+opt.text+'</text>'
-        + '</svg>\') '+opt.cx+' '+opt.cy+', auto';
-
-    return svg;
+    
+  
+    var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+    
+    canvas.width = opt.width;
+    canvas.height = opt.height;
+  
+    if (opt.backgroundColor) {
+        ctx.fillStyle = opt.backgroundColor;
+        ctx.fillRect(0, 0, opt.width, opt.height);
+    }
+    
+    ctx.translate(opt.rotateOriginX, opt.rotateOriginY);
+    ctx.rotate(opt.rotate);
+    ctx.translate(-opt.rotateOriginX, -opt.rotateOriginY);
+  
+    ctx.font = opt.fontSize + ' ' + opt.fontFamily;
+    ctx.textBaseline = 'top';
+  
+    if (opt.stroke) {
+        ctx.strokeStyle = opt.stroke.color;
+        ctx.lineWidth = opt.stroke.width;
+        ctx.strokeText(opt.text, opt.textX, opt.textY);
+    }
+    
+    ctx.fillStyle = opt.color;
+    ctx.fillText(opt.text, opt.textX, opt.textY);
+  
+    if (opt.hotspotColor) {
+        ctx.fillStyle = opt.hotspotColor;
+        ctx.fillRect(opt.hotspotX, opt.hotspotY, 1, 1);
+    }
+    
+    var uri = canvas.toDataURL();
+ 
+    return 'url(\'' + uri + '\') '+opt.hotspotX+' '+opt.hotspotY+', auto';
 }

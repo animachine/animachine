67113fa4f1a4972d0898238ad45f41485d431862
azazdeaz <azazdeaz@gmail.com>
2015-01-20 17:59:49 +0100

    fix extension break bug

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ gulp.task('vendor', function () {
       paths.assets + 'js/webfont.js',
       paths.node + 'css.escape/css.escape.js',
       paths.node + 'whatwg-fetch/fetch.js',
+      paths.bower + 'Array.prototype.find/index.js',
       paths.bower + 'lodash/dist/lodash.min.js',
       paths.bower + 'gsap/src/minified/TweenMax.min.js',
       paths.bower + 'jquery/dist/jquery.js',

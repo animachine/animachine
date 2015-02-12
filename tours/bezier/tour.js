358e2937d032c9d288c5014b989a71870812e5c3
azazdeaz <azazdeaz@gmail.com>
2015-02-12 19:09:14 +0100

    init bezier tour

---
+++ tours/bezier/tour.js
@@ -0,0 +1,109 @@
+'use strict';
+
+$(function () {
+
+    am.open();
+    setupWorkspace();
+
+    am.tour.setup({
+
+        steps: [
+            {
+                title: 'Hi!',
+                content: getStep0Content(),
+            },
+            {
+                title: 'Turn to bezier!',
+                content: getStep1Content(),
+                checklist: [
+                    'turn to bezier',
+                    'add an extra point to it',
+                ],
+                runningLoop: function (tour) {
+
+                    var save;
+
+                    if (!tour.isChecked(0) || !tour.isChecked(1)) {
+
+                        save = am.projectMap.currProject.getSave();
+                    }
+
+                    if (!tour.isChecked(0)) {
+
+                        if(/"bezier"/.test(JSON.stringify(save))) {
+                            tour.checkIn(0);
+                        }
+                    }
+                    if (!tour.isChecked(1)) {
+
+                        walkObj(save, function (value, key) {
+
+                            if (key === 'value' &&
+                                value instanceof Array &&
+                                value.length > 1)
+                            {
+                                tour.checkIn(1);
+                            }
+                        });
+                    }
+                }
+            },
+            {
+                title: 'Done!)',
+                content: getStep2Content(),
+            },
+        ],
+    });
+});
+
+
+function walkObj(obj, cb) {
+
+    return Object.keys(obj).some(function (key) {
+
+        var value = obj[key];
+
+        if (cb(value, key, obj)) {
+            return true;
+        }
+
+        if (typeof(value) === 'object') {
+
+            return walkObj(value, cb);
+        }
+    });
+}
+
+
+function getStep0Content() {
+
+    return '<img src="http://zippy.gfycat.com/MelodicPerfectAlpinegoat.gif" style="width:100%;">' +
+        '<p>This a quict tutorial to the bezier paths (or motion paths if you like).</p>' +
+        '<p>On the timeline you see a track with a simple animation. If you hit the play button you\'ll see that the bullet leaves the cannon. It\'s looks pretty dummy but we can turn this point to point movement into curve</p>';
+}
+
+function getStep1Content() {
+
+    return '<img src="http://zippy.gfycat.com/PoshHighlevelFalcon.gif" style="width:100%;">' +
+        '<p>So just click on the <span class="curve"></span> button on the right of the translate group and you\'ll see a blue line on the screen which shows the root of the bullet. If you familiar with any vector graphics editor you can easily modify the path by adding, editing, removing points. If you stop your over the path, anchor or handle the hints will show up.</p>';
+}
+
+function getStep2Content() {
+
+    return '<img src="http://zippy.gfycat.com/ImpureSplendidHyracotherium.gif" style="width:100%;">' +
+        '<p>That\'s it. If you click to the <span class="chart-line"></span> you can convert bezier param beck to linear translate.</p>' +
+        '<p>Thanks for trying out the Animachine(alpha)! For more information check out the project page on <a href="https://github.com/animachine/animachine">github</a></p>';
+}
+
+function setupWorkspace() {
+
+    var ws = am.workspace.getWorkspace('base');
+    ws.children[0].children.push({
+        type: 'panel',
+        noHead: true,
+        size: 1,
+        scaleMode: 'flex',
+        tabs: [{name: 'tour'}]
+    });
+    am.workspace.load('tour', ws);
+}

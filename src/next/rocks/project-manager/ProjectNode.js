9b31a5e1a97caa58f07d8ae6c932aebff4a62810
Polgár András <azazdeaz@gmail.com>
2015-08-11 09:53:59 +0200

    control animation time

--- src/next/rocks/project-manager/ProjectNode.js
+++ src/next/rocks/project-manager/ProjectNode.js
@@ -20,12 +20,39 @@ export default class Project {
           runningAnimation._animationSource._amProjectSource === this.projectSource
         ) {
           var timelineName = runningAnimation._animationSource._amTimelineName
+          var timeline = this.model.findTimelineBy('name', timelineName)
+
           var projectSource = this.model.getSource()
-          var animationSource = createAnimationSource(projectSource, timelineName)
+          var animationSource = wrapAnimationSource(
+            createAnimationSource(projectSource, timelineName),
+            timeline
+          )
           animationSource._amProject = this
+          animationSource._amTimelineName = timelineName
           runningAnimation.replaceAnimationSource(animationSource)
         }
       })
     })
   }
 }
+
+const wrapAnimationSource = (() => {
+  var disposeLast
+
+  return (animationSource, timeline) => {
+    return (...args) => {
+      const gsTimeline = animationSource(...args)
+      const setTime = () => gsTimeline.time(timeline.currentTime / 1000)
+      gsTimeline.pause()
+      setTime()
+      timeline.on('change.currentTime', setTime)
+
+      if (disposeLast) {
+        disposeLast()
+      }
+      disposeLast = () => timeline.off('change.currentTime', setTime)
+
+      return gsTimeline
+    }
+  }
+}())

a156e15c92193dcb5aaf74eb0009af5086bc3b89
Polgár András <azazdeaz@gmail.com>
2015-09-02 18:22:32 +0200

    create browser build

---
+++ webpack.browser.config.js
@@ -0,0 +1,29 @@
+var path = require('path')
+
+module.exports = {
+  context: __dirname,
+  devtool: 'eval',
+  entry: path.join(__dirname, 'src/next/index.js'),
+  output: {
+    publicPath: '/',
+    path: path.join( __dirname, '/browser'),
+    filename: 'animachine.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'react-animachine-enhancer': path.join(__dirname, 'src/next/react-animachine-enhancer'),
+      'react': path.join(__dirname, './node_modules/react'),
+      'custom-drag': 'react-matterkit/lib/custom-drag',
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}

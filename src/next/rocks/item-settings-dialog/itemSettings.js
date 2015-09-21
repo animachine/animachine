c7829c0d307ffdb85e5fec9f06ea04d5a7010f3a
Polgár András <azazdeaz@gmail.com>
2015-09-21 08:14:34 +0200

    wip: item-settings-dalog

--- src/next/rocks/item-settings-dialog/itemSettings.js
+++ src/next/rocks/item-settings-dialog/itemSettings.js
@@ -14,23 +14,36 @@ const getWhitelist = type => {
     case 'track':
       return ['name']
     case 'param':
-      return ['name']
+      return ['name', 'params']
 
   }
 }
 
+function createSelector(itemType, key) {
+  return connect => (
+    connect.parent &&
+    connect.parent.type === itemType &&
+    connect.key === key
+  )
+}
+
 function createInputSettingsNode(itemType, key, onChange) {
   return {
-    selector: connect => (
-      connect.parent &&
-      connect.parent.type === itemType &&
-      connect.key === key
-    ),
+    selector: createSelector(itemType, key),
     input: {
       onChange(value, connect) {
         const {actions} = BETON.getRock('project-manager')
         onChange(value, connect.parent.id, actions)
       }
+    }
+  }
+}
+function createChildSettingsNode(itemType, childrenName) {
+  return {
+    selector: createSelector(itemType, key),
+    label: `child ${childrenName}s`,
+    settings: {
+      children: null
     }
   }
 }

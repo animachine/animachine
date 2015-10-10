8f587c74f15f0d49ad1c301af7b1e09ef0cb4ffc
Polgár András <azazdeaz@gmail.com>
2015-10-10 15:39:23 +0200

    remove console.log

--- src/next/rocks/project-manager/index.js
+++ src/next/rocks/project-manager/index.js
@@ -1,7 +1,7 @@
 import * as selectors from './selectors'
 import * as actions from './actions'
 import reducer from './reducer'
-console.log('QQQQQQQQQQQQQQQQactions', actions)
+
 BETON.define({
   id: 'project-manager',
   dependencies: ['store'],

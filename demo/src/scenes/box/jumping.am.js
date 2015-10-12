ea213b083fa9d733443750cc71a82aa6b5b0ea68
Polgár András <azazdeaz@gmail.com>
2015-10-12 19:37:07 +0200

    new box demo anim

--- demo/src/scenes/box/jumping.am.js
+++ demo/src/scenes/box/jumping.am.js
@@ -1,42 +1,460 @@
-import {createAnimationSource} from 'react-animachine-enhancer'
+var animachineEnhancer = require('react-animachine-enhancer')
+var createAnimationSource = animachineEnhancer.createAnimationSource
 
-const projectSource = {
-  timelines: [
-    {
-      name: 'jump',
-      length: 2000,
-      tracks: [
-        {
-          name: 'box',
-          selectors: [[]],
-          params: [
-            {
-              name: 'x',
-              keys: [
-                {time: 0, value: 100},
-                {time: 80, value: 0},
-                {time: 160, value: 200},
-              ]
+var projectSource = {
+    'currentTimelineId': '17',
+    'timelines': [{
+        'name': 'jump',
+        'isPlaying': false,
+        'currentTime': 253,
+        'length': 2000,
+        'pxpms': 0.25,
+        'width': 2000,
+        'start': 23,
+        'startMargin': 6,
+        'currentTrackId': '16',
+        'inlineEaseEditor': {
+            'top': 0,
+            'height': 21,
+            'targetKeyId': '35',
+            'controlledEaseIds': ['36', '38']
+        },
+        'tracks': [{
+            'name': 'box',
+            'selectors': [
+                []
+            ],
+            'openInTimeline': true,
+            'showThreeDimensionalParams': false,
+            'params': [{
+                'name': 'x',
+                'openInTimeline': true,
+                'keys': [{
+                    'time': 0,
+                    'value': 299.48168963405965,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '1',
+                        'type': 'ease'
+                    },
+                    'id': '2',
+                    'type': 'key'
+                }, {
+                    'time': 972,
+                    'value': 299.48168963405965,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '44',
+                        'type': 'ease'
+                    },
+                    'id': '43',
+                    'type': 'key'
+                }],
+                'id': '7',
+                'type': 'param'
             }, {
-              name: 'y',
-              keys: [
-                {time: 0, value: 100},
-                {time: 80, value: 200},
-                {time: 160, value: 100},
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  ]
+                'name': 'y',
+                'openInTimeline': true,
+                'keys': [{
+                    'time': 0,
+                    'value': 429.3705022907716,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '8',
+                        'type': 'ease'
+                    },
+                    'id': '9',
+                    'type': 'key'
+                }, {
+                    'time': 972,
+                    'value': 429.3705022907716,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '46',
+                        'type': 'ease'
+                    },
+                    'id': '45',
+                    'type': 'key'
+                }, {
+                    'time': 1492,
+                    'value': 116.37050229077158,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.1769230769230769,
+                        'pointAY': 0.7285714285714285,
+                        'pointBX': 0.4923076923076922,
+                        'pointBY': 0.9857142857142857,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '48',
+                        'type': 'ease'
+                    },
+                    'id': '47',
+                    'type': 'key'
+                }, {
+                    'time': 2024,
+                    'value': 429.3705022907716,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.46541353383458645,
+                        'pointAY': 0.06190476190476191,
+                        'pointBX': 0.8654135338345864,
+                        'pointBY': 0.08095238095238089,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '50',
+                        'type': 'ease'
+                    },
+                    'id': '49',
+                    'type': 'key'
+                }],
+                'id': '14',
+                'type': 'param'
+            }, {
+                'name': 'transformOriginX',
+                'openInTimeline': true,
+                'keys': [{
+                    'time': 0,
+                    'value': 0.5,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '22',
+                        'type': 'ease'
+                    },
+                    'id': '21',
+                    'type': 'key'
+                }],
+                'id': '19',
+                'type': 'param'
+            }, {
+                'name': 'transformOriginY',
+                'openInTimeline': true,
+                'keys': [{
+                    'time': 0,
+                    'value': 1,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '24',
+                        'type': 'ease'
+                    },
+                    'id': '23',
+                    'type': 'key'
+                }],
+                'id': '20',
+                'type': 'param'
+            }, {
+                'name': 'scaleX',
+                'openInTimeline': true,
+                'keys': [{
+                    'time': 0,
+                    'value': 1,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '28',
+                        'type': 'ease'
+                    },
+                    'id': '27',
+                    'type': 'key'
+                }, {
+                    'time': 720,
+                    'value': 1.6150102153435528,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.17222222222222222,
+                        'pointAY': 0.49047619047619045,
+                        'pointBX': 0.47777777777777775,
+                        'pointBY': 0.9380952380952381,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '34',
+                        'type': 'ease'
+                    },
+                    'id': '33',
+                    'type': 'key'
+                }, {
+                    'time': 1420,
+                    'value': 0.9,
+                    'selected': true,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.14485784485784484,
+                        'pointAY': 0.9666666666666666,
+                        'pointBX': 0.6805194805194805,
+                        'pointBY': 1.0333333333333332,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '36',
+                        'type': 'ease'
+                    },
+                    'id': '35',
+                    'type': 'key'
+                }, {
+                    'time': 804,
+                    'value': 1.6150102153435528,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '42',
+                        'type': 'ease'
+                    },
+                    'id': '41',
+                    'type': 'key'
+                }, {
+                    'time': 1868,
+                    'value': 1,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '54',
+                        'type': 'ease'
+                    },
+                    'id': '53',
+                    'type': 'key'
+                }],
+                'id': '25',
+                'type': 'param'
+            }, {
+                'name': 'scaleY',
+                'openInTimeline': true,
+                'keys': [{
+                    'time': 0,
+                    'value': 1,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '30',
+                        'type': 'ease'
+                    },
+                    'id': '29',
+                    'type': 'key'
+                }, {
+                    'time': 720,
+                    'value': 0.2565560638035217,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.21111111111111108,
+                        'pointAY': 0.49047619047619045,
+                        'pointBX': 0.3555555555555555,
+                        'pointBY': 0.8428571428571427,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '32',
+                        'type': 'ease'
+                    },
+                    'id': '31',
+                    'type': 'key'
+                }, {
+                    'time': 1420,
+                    'value': 1.29,
+                    'selected': true,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.14485784485784484,
+                        'pointAY': 0.9666666666666666,
+                        'pointBX': 0.6805194805194805,
+                        'pointBY': 1.0333333333333332,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '38',
+                        'type': 'ease'
+                    },
+                    'id': '37',
+                    'type': 'key'
+                }, {
+                    'time': 804,
+                    'value': 0.2565560638035217,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '40',
+                        'type': 'ease'
+                    },
+                    'id': '39',
+                    'type': 'key'
+                }, {
+                    'time': 1868,
+                    'value': 1,
+                    'selected': false,
+                    'ease': {
+                        'easeType': 'bezier',
+                        'pointAX': 0.3,
+                        'pointAY': 0.3,
+                        'pointBX': 0.7,
+                        'pointBY': 0.7,
+                        'roughEase': false,
+                        'roughStrength': 1,
+                        'roughPoints': 20,
+                        'roughClamp': false,
+                        'roughRandomise': true,
+                        'roughTaper': 'none',
+                        'id': '52',
+                        'type': 'ease'
+                    },
+                    'id': '51',
+                    'type': 'key'
+                }],
+                'id': '26',
+                'type': 'param'
+            }],
+            'id': '16',
+            'type': 'track'
+        }],
+        'id': '17',
+        'type': 'timeline'
+    }],
+    'id': '18',
+    'type': 'project'
 }
+var animations = {}
 
-const animations = {}
-
-projectSource.timelines.forEach(timeline => {
+projectSource.timelines.forEach(function (timeline) {
   var gsapSource = createAnimationSource({projectSource, timeline})
   animations[timeline.name] = gsapSource
 })
 
-export default animations
+module.exports = animations
---
+++ demo/src/scenes/box/jumping2.am.js
@@ -0,0 +1,42 @@
+import {createAnimationSource} from 'react-animachine-enhancer'
+
+const projectSource = {
+  timelines: [
+    {
+      name: 'jump',
+      length: 2000,
+      tracks: [
+        {
+          name: 'box',
+          selectors: [[]],
+          params: [
+            {
+              name: 'x',
+              keys: [
+                {time: 0, value: 100},
+                {time: 80, value: 0},
+                {time: 160, value: 200},
+              ]
+            }, {
+              name: 'y',
+              keys: [
+                {time: 0, value: 100},
+                {time: 80, value: 200},
+                {time: 160, value: 100},
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+const animations = {}
+
+projectSource.timelines.forEach(timeline => {
+  var gsapSource = createAnimationSource({projectSource, timeline})
+  animations[timeline.name] = gsapSource
+})
+
+export default animations

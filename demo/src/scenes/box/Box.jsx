8243c35cd416f292bd138020188b17223ccdf514
Polgár András <azazdeaz@gmail.com>
2015-10-18 15:18:32 +0200

    update box demo

--- demo/src/scenes/box/Box.jsx
+++ demo/src/scenes/box/Box.jsx
@@ -11,8 +11,8 @@ export default class Box extends React.Component {
   render() {
     var style = {
       backgroundColor: '#0074D9',
-      width: '160px',
-      height: '160px',
+      width: '80px',
+      height: '80px',
     }
     return <div key='box' style={style}/>
   }

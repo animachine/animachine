106251c5408e175306be5825b0c2304bc431c7a9
Polgár András <azazdeaz@gmail.com>
2016-05-29 17:36:55 +0200

    fix deploying

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -1,8 +1,6 @@
 require('./index.html') //for the webpack build
 require('./styles.css')
-
-// import injectTapEventPlugin from 'react-tap-event-plugin'
-// injectTapEventPlugin()
+require('file?name=[name]!./CNAME')
 
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -15,7 +13,7 @@ import Demo from './Demo'
 ReactDOM.render(<Router history={browserHistory}>
   <Route component={App}>
     {/*<Route path="about" component={About}/>*/}
-    <Route path="demo/:name" component={Demo}/>
+    <Route path=":name" component={Demo}/>
     {/*<Redirect from='*' to='/demo/Box' />*/}
   </Route>
 </Router>, document.querySelector('#react-mount'))

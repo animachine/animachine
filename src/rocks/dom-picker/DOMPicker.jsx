e601f7ba753139a37d8efdec4a817481267d370b
Polgár András <azazdeaz@gmail.com>
2016-05-29 18:16:27 +0200

    fix selector generation

--- src/rocks/dom-picker/DOMPicker.jsx
+++ src/rocks/dom-picker/DOMPicker.jsx
@@ -47,7 +47,7 @@ export default class DomPicker extends React.Component {
 
   addNewTrack(node) {
     const {state} = BETON.require('project-manager')
-    const qurey = this.selector()
+    const query = this.selector()
     const track = state.currentTimeline.addTrack({
       name: `new track for "${query}"`,
       selectors: [{type: 'css', query}],
